0a37050cfba4cad095a11201259b39aec13f6ea3
Ania <anna.kors.19.1@gmail.com>
2025-08-31 19:37:31 +0300

    Fix file-encoder.py

--- file-encoder.py
+++ file-encoder.py
@@ -6,15 +6,15 @@ output_file = "top-rated-coffee-clean_fixed.csv"
 
 # відкрити файл з utf-8, якщо не вийде — latin1
 try:
-    df = pd.read_csv(input_file, encoding="utf-8", sep=';', on_bad_lines='skip')
-    print("Файл відкрито з кодуванням utf-8")
-except UnicodeDecodeError:
     df = pd.read_csv(input_file, encoding="latin1", sep=';', on_bad_lines='skip')
     print("Файл відкрито з кодуванням latin1")
+except UnicodeDecodeError:
+    df = pd.read_csv(input_file, encoding="utf-8", sep=';', on_bad_lines='skip')
+    print("Файл відкрито з кодуванням utf-8")
 
 # перевірка
-print(df.head())
+#print(df.head(30))
 
 # збереження
-#df.to_csv(output_file, encoding="utf-8", sep=';', index=False)
-#print(f"Виправлений файл збережено як '{output_file}'")
+df.to_csv(output_file, encoding="latin1", sep=';', index=False)
+print(f"Виправлений файл збережено як '{output_file}'")
