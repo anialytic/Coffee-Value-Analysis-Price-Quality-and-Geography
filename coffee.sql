3d616d0127a5ed694b4691d13f6811684182b3a4
Ania <anna.kors.19.1@gmail.com>
2025-09-25 16:04:24 +0300

    Add query with window function to find top coffee per country

--- coffee.sql
+++ coffee.sql
@@ -23,3 +23,18 @@ from coffee
 where origin_country_clean is not null
 group by origin_country_clean
 order by coffee_nmb desc
+
+--coffee with max_score in each country
+select coffee_name,
+       total_score,
+       origin_country_clean
+from(
+    select coffee_name,
+           total_score,
+           origin_country_clean,
+           row_number() over (partition by origin_country_clean order by total_score desc) as rn
+    from coffee
+    where origin_country_clean is not null
+) t
+where rn = 1
+order by total_score desc
