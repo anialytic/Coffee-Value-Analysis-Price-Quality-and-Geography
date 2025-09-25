3047275f0f639e0c113f19cb44cd282b4917666c
Ania <anna.kors.19.1@gmail.com>
2025-09-25 15:43:24 +0300

    Add average score by origin_country

--- coffee.sql
+++ coffee.sql
@@ -7,3 +7,11 @@ FROM coffee
 WHERE price_per_100g IS NOT NULL
 ORDER BY price_per_100g DESC
 LIMIT 10
+
+--average score by countries
+select avg(total_score) as avg_score,
+	origin_country_clean
+from coffee
+where origin_country_clean is not null
+group by origin_country_clean
+order by avg_score desc
