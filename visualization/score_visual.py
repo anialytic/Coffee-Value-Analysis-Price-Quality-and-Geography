83326428e12fd6bd73fdf28a82e2188989ddeb89
Ania <anna.kors.19.1@gmail.com>
2025-10-10 18:03:39 +0300

    Update score histogram

--- visualization/score_visual.py
+++ visualization/score_visual.py
@@ -1,19 +1,32 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 df = pd.read_csv("coffee-cleaned.csv", sep=';')
 scores = df["total_score"]
 
 #plot histogram
-plt.figure(figsize=(8,6))
-plt.hist(scores, bins=10, color="pink", edgecolor="brown") 
+plt.figure(figsize=(9,6))
+n, bins, patches = plt.hist(scores, bins=15, color="#A6705D", edgecolor="#A6705D", alpha=0.8)
 
-#labels 
-plt.xlabel("Coffee Quality Score")
-plt.ylabel("Number of coffee")
-plt.title("Distribution of Coffee Quality (Histogram)")
+#add mean
+mean_score = scores.mean()
+plt.axvline(mean_score, color='#402927', linestyle='--', linewidth=2, label=f'Mean = {mean_score:.2f}')
 
-#limit range
-plt.xticks(range(93,100)) 
-plt.xlim(93, 100) 
+#title and labels
+plt.title("Distribution of Coffee Quality Scores", fontsize=16, fontweight='bold')
+plt.xlabel("Coffee Quality Score", fontsize=13)
+plt.ylabel("Number of Samples", fontsize=13)
+
+# limits
+plt.xlim(scores.min()-0.5, scores.max()+0.5)
+plt.xticks(np.arange(int(scores.min()), int(scores.max())+1, 1))
+
+# grin&legend
+plt.grid(True, linestyle='--', alpha=0.6)
+plt.legend()
+
+#add source text
+plt.figtext(0.95, 0.01, 'Source:https://www.coffeereview.com/highest-rated-coffees/', ha = 'right', fontsize=9, color = 'gray')
+
 plt.show()
