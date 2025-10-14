1b2610f6a0280065e543d570c2902adc782ac2b1
Ania <anna.kors.19.1@gmail.com>
2025-10-14 12:55:33 +0300

    Update descriptive statistics in py

--- analysis/coffee_stats.py
+++ analysis/coffee_stats.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import statistics as statistics
 df = pd.read_csv('coffee-cleaned.csv', sep=';')
 
 # check column names
@@ -9,7 +10,49 @@ df = pd.read_csv('coffee-cleaned.csv', sep=';')
 
 # check the cheapest coffee
 min_val = np.min(df["price_per_100g"].dropna())
-print(min_val)
+print('min:', min_val)
 
 max_val = np.max(df["price_per_100g"].dropna())
-print(max_val)
+print('max:', max_val)
+
+mean = np.mean(df['price_per_100g'].dropna())
+print('mean:', mean)
+
+median = np.median(df['price_per_100g'].dropna())
+print('median:', median)
+
+mode = statistics.mode(df['price_per_100g'].dropna())
+print('mode:', mode)
+
+range = max_val - min_val
+print('range:', range)
+
+five_num = [
+    float(df['price_per_100g'].quantile(0)), 
+    float(df['price_per_100g'].quantile(0.25)), 
+    float(df['price_per_100g'].quantile(0.5)), 
+    float(df['price_per_100g'].quantile(0.75)), 
+    float(df['price_per_100g'].quantile(1))]
+print(five_num)
+
+describe = df['price_per_100g'].describe()
+print(describe)
+
+IQR = float(df['price_per_100g'].quantile(0.75)) - float(df['price_per_100g'].quantile(0.25))
+print('IQR:', IQR)
+
+variance = np.var(df['price_per_100g'].dropna())
+print('variance:', variance)
+
+deviation = np.std(df['price_per_100g'].dropna())
+print('standart deviation:', deviation)
+
+abs_median_devs = abs(df['price_per_100g'].median() - df['price_per_100g'])
+abs_median_devs = abs_median_devs.median()
+print('abs_median_devs:', abs_median_devs)
+
+skewness = df['price_per_100g'].skew()
+print('skewness:', skewness)
+
+kurtosis = df['price_per_100g'].kurt()
+print('kurtosis:', kurtosis)
