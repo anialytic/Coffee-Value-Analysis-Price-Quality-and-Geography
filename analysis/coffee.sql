7400daa9bf959554d263f8e7b7ac7c729d9cc320
Ania <anna.kors.19.1@gmail.com>
2025-11-18 19:02:56 +0200

    Fix sql query

--- analysis/coffee.sql
+++ analysis/coffee.sql
@@ -4,48 +4,53 @@ SELECT * FROM coffee;
 SELECT coffee_name,
 	price_per_100g
 FROM coffee
-WHERE price_per_100g IS NOT NULL
 ORDER BY price_per_100g DESC
 LIMIT 10
 
+
+-- Fixing 
+UPDATE coffee
+SET origin_country = 'USA'
+WHERE origin_country = 'USa';
+
 --average score by countries
 select avg(total_score) as avg_score,
-	origin_country_clean
+	origin_country
 from coffee
-where origin_country_clean is not null
-group by origin_country_clean
+where origin_country is not null
+group by origin_country
 order by avg_score desc
 
 --total amount of coffee by country
 select count(coffee_name) as coffee_nmb,
-	origin_country_clean
+	origin_country
 from coffee
-where origin_country_clean is not null
-group by origin_country_clean
+where origin_country is not null
+group by origin_country
 order by coffee_nmb desc
 
 --coffee with max_score in each country
 select coffee_name,
        total_score,
-       origin_country_clean
+       origin_country
 from(
     select coffee_name,
            total_score,
-           origin_country_clean,
-           row_number() over (partition by origin_country_clean order by total_score desc) as rn
+           origin_country,
+           row_number() over (partition by origin_country order by total_score desc) as rn
     from coffee
-    where origin_country_clean is not null
+    where origin_country is not null
 ) t
 where rn = 1
 order by total_score desc
 
 -- top 3 countries with avg_price per 100g higher
 select 
-	origin_country_clean, 
+	origin_country, 
 	avg(price_per_100g) as avg_price
 from coffee
-where origin_country_clean is not null
-group by origin_country_clean
+where origin_country is not null
+group by origin_country
 having avg(price_per_100g) > 
 (
 select 
@@ -57,19 +62,19 @@ limit 3
 
 -- countries with coffee amount > 20 + average score
 select 
-	origin_country_clean
+	origin_country
 	, avg(total_score) as avg_score
 	, count(distinct coffee_name) as coffee_nmb
 from coffee
-where origin_country_clean is not null
-group by origin_country_clean
+where origin_country is not null
+group by origin_country
 having count(distinct coffee_name) > 20
 
 --avg_price for medium light per each country
 select 
-	origin_country_clean
+	origin_country
 	, avg(price_per_100g) as avg_price
 from coffee
-where roast_level = 'Medium Light' and origin_country_clean is not null
-group by origin_country_clean
+where roast_level = 'Medium Light' and origin_country is not null
+group by origin_country
 order by avg_price desc
