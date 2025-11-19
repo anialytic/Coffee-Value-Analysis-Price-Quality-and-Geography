cc268fe3c73096b82029e08a688bf7b19dd75b28
Ania <anna.kors.19.1@gmail.com>
2025-11-19 16:11:20 +0200

    Update comma_to_dot.py

--- data/processed/comma_to_dot.py
+++ data/processed/comma_to_dot.py
@@ -1,17 +1,22 @@
 import pandas as pd
-df = pd.read_csv('coffee-cleaned.csv', sep=';', quotechar='"', engine='python')
+
+csv = input('Please enter csv-file name: ')
+sep = input('Plase enter separator: ')
+
+df = pd.read_csv(csv, sep=sep, quotechar='"', engine='python')
 
 # перевірка колонок
 #print(df.columns)
+column = input('Please enter column name: ')
 
 # заміна , на .
-df['price_per_100g'] = df['price_per_100g'].astype(str).str.replace(",", ".").astype(float)
+df[column] = df[column].astype(str).str.replace(",", ".").astype(float)
 
 # на числ формат
-df['price_per_100g'] = pd.to_numeric(df['price_per_100g'], errors='coerce')
+df[column] = pd.to_numeric(df[column], errors='coerce')
 
 # перевірка
 #print(df["price_per_100g"].head())
 
 # збереження
-df.to_csv('coffee_fixed.csv', index=False)
+df.to_csv('dot.fixed.csv', index=False)
