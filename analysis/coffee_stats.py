b2927ebebecb34f904330bf6b6e0d5d51635cbd0
Ania <anna.kors.19.1@gmail.com>
2025-11-20 18:12:41 +0200

    Update the code

--- analysis/coffee_stats.py
+++ analysis/coffee_stats.py
@@ -1,58 +1,63 @@
 import numpy as np
 import pandas as pd
 import statistics as statistics
-df = pd.read_csv('coffee-cleaned.csv', sep=';')
+
+csv = input('Please enter csv-file name:')
+sep = input('Please enter separator:')
+df = pd.read_csv(csv, sep=sep)
 
 # check column names
 #print(df.columns.tolist())
 # look through "price_per_100g" column ignoring Nan
 #print(df["price_per_100g"].dropna())
 
+column=input('Please enter the column name: ')
+
 # check the cheapest coffee
-min_val = np.min(df["price_per_100g"].dropna())
+min_val = np.min(df[column].dropna())
 print('min:', min_val)
 
-max_val = np.max(df["price_per_100g"].dropna())
+max_val = np.max(df[column].dropna())
 print('max:', max_val)
 
-mean = np.mean(df['price_per_100g'].dropna())
+mean = np.mean(df[column].dropna())
 print('mean:', mean)
 
-median = np.median(df['price_per_100g'].dropna())
+median = np.median(df[column].dropna())
 print('median:', median)
 
-mode = statistics.mode(df['price_per_100g'].dropna())
+mode = statistics.mode(df[column].dropna())
 print('mode:', mode)
 
 range = max_val - min_val
 print('range:', range)
 
 five_num = [
-    float(df['price_per_100g'].quantile(0)), 
-    float(df['price_per_100g'].quantile(0.25)), 
-    float(df['price_per_100g'].quantile(0.5)), 
-    float(df['price_per_100g'].quantile(0.75)), 
-    float(df['price_per_100g'].quantile(1))]
+    float(df[column].quantile(0)), 
+    float(df[column].quantile(0.25)), 
+    float(df[column].quantile(0.5)), 
+    float(df[column].quantile(0.75)), 
+    float(df[column].quantile(1))]
 print(five_num)
 
-describe = df['price_per_100g'].describe()
+describe = df[column].describe()
 print(describe)
 
-IQR = float(df['price_per_100g'].quantile(0.75)) - float(df['price_per_100g'].quantile(0.25))
+IQR = float(df[column].quantile(0.75)) - float(df[column].quantile(0.25))
 print('IQR:', IQR)
 
-variance = np.var(df['price_per_100g'].dropna())
+variance = np.var(df[column].dropna())
 print('variance:', variance)
 
-deviation = np.std(df['price_per_100g'].dropna())
+deviation = np.std(df[column].dropna())
 print('standart deviation:', deviation)
 
-abs_median_devs = abs(df['price_per_100g'].median() - df['price_per_100g'])
+abs_median_devs = abs(df[column].median() - df[column])
 abs_median_devs = abs_median_devs.median()
 print('abs_median_devs:', abs_median_devs)
 
-skewness = df['price_per_100g'].skew()
+skewness = df[column].skew()
 print('skewness:', skewness)
 
-kurtosis = df['price_per_100g'].kurt()
+kurtosis = df[column].kurt()
 print('kurtosis:', kurtosis)
