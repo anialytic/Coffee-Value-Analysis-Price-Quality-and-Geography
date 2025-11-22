ac09037708da0cafba07d6de8d2c113d272e3227
Ania <anna.kors.19.1@gmail.com>
2025-11-22 19:28:08 +0200

    Update analysis

---
+++ coffee.R
@@ -0,0 +1,78 @@
+library(dplyr)
+library(ggplot2)
+
+coffee <- read.csv("C://Users//HP//Desktop//Coffee//data//cleaned//coffee_filled.csv", header = TRUE, sep = ';')
+
+head(coffee)
+
+# описова статистика
+mean(coffee$price_per_100g)   
+class(coffee$price_per_100g)  
+median(coffee$price_per_100g, na.rm = TRUE) 
+min(coffee$price_per_100g, na.rm = TRUE)    
+max(coffee$price_per_100g, na.rm = TRUE)
+range(coffee$price_per_100g, na.rm = TRUE) 
+var(coffee$price_per_100g, na.rm = TRUE)  
+sd(coffee$price_per_100g, na.rm = TRUE)   
+summary(coffee$price_per_100g)   
+
+# вивести топ-10 найдорожчих видів кави
+coffee %>%
+    slice_max(n=10, price_per_100g) %>%
+    arrange(desc(price_per_100g))
+
+# порахувати, де найбільше записів + графік (цей графік стосується roaster_country)
+country_counts <- as.data.frame(table(coffee$roaster_country))
+ggplot(country_counts, aes(x = reorder(Var1, -Freq), y = Freq)) +
+  geom_bar(stat = "identity", fill = "pink") +
+  labs(x = "Країна", y = "Кількість", title = "Кількість кави по країнах") +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1))
+
+# гіпотеза: найдорожчі види кави пов'язані з регіонами: Гватемала та Ефіопія
+# Н0: ціни в Гватемалі/Ефіопії не відрізняються від інших країн
+# Н1: ціни там вищі
+# Результат: Н0, Н1 частково підтверджується, але найдорощі лоти часто з Панами,Еквадору
+
+# середнє, медіана, максимум. найвища середня у Тайвані, 
+coffee %>%
+  group_by(origin_country) %>%
+  summarise(
+    mean_price = mean(price_per_100g, na.rm = TRUE),
+    median_price = median(price_per_100g, na.rm = TRUE),
+    max_price = max(price_per_100g, na.rm = TRUE),
+    n = n()
+  ) %>%
+  arrange(desc(median_price))
+
+# Коробкова візуалізація за Гватемалою, Ефіопією, Тайванем
+#у Гватемали та Ефіопії є екстримальні значення(ексклюзивні лоти), які тягнуть вгору
+coffee %>%
+  filter(origin_country %in% c("Guatemala", "Ethiopia", "Taiwan")) %>%
+  ggplot(aes(x = origin_country, y = price_per_100g)) +
+  geom_boxplot() +
+  labs(title = "Coffee prices in Guatemala and Ethiopia",
+       x = "Country of Origin",
+       y = "Price per 100g")
+
+# t-test, p-value. на 95% на 6.63 доларів дорожча з Гватемали, Ефіопії
+coffee %>%
+  mutate(region = ifelse(origin_country %in% c("Guatemala", "Ethiopia"),
+                         "Guatemala/Ethiopia", "Other")) %>%
+  t.test(price_per_100g ~ region, data = ., alternative = "greater")
+
+# зріз топових лотів. найдорожча кава Панама, Еквадор
+coffee %>%
+  slice_max(order_by = price_per_100g, n = 10) %>%
+  select(coffee_name, origin_country, price_per_100g)
+
+# коробкова діаграма цін по країнам (топ 5)
+top_countries <- names(sort(table(coffee$origin_country), decreasing = TRUE))[1:5]
+df_top <- coffee[coffee$origin_country %in% top_countries, ]
+df_clean <- subset(df_top, origin_country != "" & !is.na(origin_country) & !is.na(price_per_100g))
+
+ggplot(df_clean, aes(x = origin_country, y = price_per_100g)) +
+  geom_boxplot(fill = "pink") +
+  labs(title = "Price distribution (Top 5 countries)",
+       x = "Country",
+       y = "Price") +
+  theme_minimal()
