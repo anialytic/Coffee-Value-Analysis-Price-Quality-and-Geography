ab98edfe5f16dcd89baec7fa8e6dbbbd6a18902e
Ania <anna.kors.19.1@gmail.com>
2025-11-26 18:06:34 +0200

    Fix bug

--- analysis/anomaly.py
+++ analysis/anomaly.py
@@ -2,13 +2,15 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_csv('C:\\Users\\HP\\Desktop\\Coffee\\data\\cleaned\\coffee_filled.csv', sep=';')
-num_cols = ["price_per_100g", "total_score", "agtron_ground", "agtron_roast"]
+num_cols = ["price_per_100g", "total_score", "agtron_ground", "agtron_roast"] 
 
-#Z-score
-df_z = df[num_cols].apply(lambda x: (x - x.mean()) / x.std())
+#Z-score 
+df_z = df[num_cols].apply(lambda x: (x - x.mean()) / x.std()) 
+df["is_anomaly"] = (df_z.abs() > 3).any(axis=1) 
 
-#позначити аномалії
-df["is_anomaly"] = (df_z.abs() > 3).any(axis=1)
-
-anomalies = df[df["is_anomaly"]]
+anomalies = df[df["is_anomaly"]] 
 print(anomalies)
+
+#перевірити, що не так
+#print(df[num_cols].describe())
+#print(df_z.abs().max())
